f69f0d392399866c20039b4e54081ee5d019d9ff
_ <pedropmancillac@gmail.com>
2022-07-21 21:16:31 -0400

    works

--- src/components/WorksComponent.vue
+++ src/components/WorksComponent.vue
@@ -1,16 +1,17 @@
 <template>
   <v-row id="WorksComponent" justify="center" class="mb-16">
     <v-col
-      v-for="({ title, imgs, path, description }, index) in works"
+      v-for="({ title, imgs, path, deploy, repo }, index) in works"
       :key="index"
       cols="11"
+      sm="8"
       md="5"
     >
       <v-card class="px-8 rounded-lg" elevation="6">
-        <v-card-title class="px-0" v-text="title" />
+        <v-card-title class="px-0 text-h5 text-sm-h4" v-text="title" />
         <v-carousel
           hide-delimiters
-          height="200px"
+          height="232px"
           cycle
           interval="4000"
           dark
@@ -19,19 +20,21 @@
           <v-carousel-item
             v-for="(img, i) in imgs"
             :key="i"
-            class="primary rounded"
+            class="blue-grey lighten-5 rounded"
           >
             <v-img
-              class=""
+              class="my-4"
               :src="require(`@/assets/${path}/${img}`)"
-              height="100%"
+              height="200px"
               contain
+              position="center center"
             />
           </v-carousel-item>
         </v-carousel>
-        <v-row justify="end" class="pt-4">
-          <v-card-subtitle v-text="description" />
-        </v-row>
+        <v-sheet class="d-flex justify-space-between flex-column flex-sm-row py-4">
+          <v-btn v-text="`Ver Repositorio`" :href="repo" color="secondary" text/>
+          <v-btn v-text="`Ver deploy`" :href="deploy" color="primary" text/>
+        </v-sheet>
       </v-card>
     </v-col>
   </v-row>
@@ -48,6 +51,8 @@ export default {
         path: "game-opinions-screenshots",
         imgs: ["desktop.png", "mobile.png"],
         description: "1,000 miles of wonder",
+        deploy:'https://mancillapedro.github.io/games-opinion-site/',
+        repo:'https://github.com/mancillapedro/games-opinion-site',
       },
     ],
   }),
